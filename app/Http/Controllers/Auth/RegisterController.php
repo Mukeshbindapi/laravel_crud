bc5d97a82c1366c5d996b1a1e92babc945c916a8
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-07-01 09:21:06 +0530

    26_Password_Hashing

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -13,10 +14,11 @@ class RegisterController extends Controller
     }
 
     public function store(Request $request){
+        $password = Hash::make($request->password);
         $user = new User();
         $user->name = $request->name;
         $user->email = $request->email;
-        $user->password = md5($request->password);
+        $user->password = $password;
         $user->gender = $request->gender;
         $user->phone = $request->phone;
         $user->address = $request->address;
